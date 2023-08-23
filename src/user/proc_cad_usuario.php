f9b1ea8c897778185b57cb37bed3aaaa7d2cace8
Josué Marques <39627456+jmwho@users.noreply.github.com>
2023-08-22 23:45:01 -0300

    Update proc_cad_usuario.php
    Agora a tela de cadastro de usuário consulta no banco de dados se já existe um usuário igual do campo "usuário" já cadastrado e informa a quem estiver cadastrando.

--- src/user/proc_cad_usuario.php
+++ src/user/proc_cad_usuario.php
@@ -3,8 +3,8 @@ session_start();
 
 // Verifique se o usuário está logado
 if (!isset($_SESSION['usuario_id'])) {
-    header("Location: ../../login.php");
-    exit();
+	header("Location: ../../login.php");
+	exit();
 }
 
 include_once("../../conexao.php");
@@ -14,16 +14,40 @@ $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+// Verifique se o usuário já existe
+$stmt_verificar = mysqli_prepare($conn, "SELECT id FROM usuarios WHERE usuario = ?");
+mysqli_stmt_bind_param($stmt_verificar, "s", $usuario);
+mysqli_stmt_execute($stmt_verificar);
+mysqli_stmt_store_result($stmt_verificar);
+
+if (mysqli_stmt_num_rows($stmt_verificar) > 0) {
+	// O usuário já existe, redirecione de volta para a página de cadastro com uma mensagem de erro
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário '$usuario' já existe</p>";
+	header("Location: cad_usuario.php");
+	exit();
+}
+
 // Criptografar a senha usando password_hash
 $senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
 
-$result_usuario = "INSERT INTO usuarios (nome, email, usuario, senha, modificado) VALUES ('$nome', '$email', '$usuario', '$senhaCriptografada', NOW())";
-$resultado_usuario = mysqli_query($conn, $result_usuario);
+// Prepare a consulta SQL para inserir o novo usuário
+$stmt = mysqli_prepare($conn, "INSERT INTO usuarios (nome, email, usuario, senha, modificado, criado) VALUES (?, ?, ?, ?, NOW(), NOW())");
 
-if (mysqli_insert_id($conn)) {
-	$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
-	header("Location: index.php");
-} else {
-	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
-	header("Location: cad_usuario.php");
+if ($stmt) {
+	mysqli_stmt_bind_param($stmt, "ssss", $nome, $email, $usuario, $senhaCriptografada);
+	$resultado_usuario = mysqli_stmt_execute($stmt);
+
+	if ($resultado_usuario) {
+		$_SESSION['msg'] = "<p style='color:green;'>Usuário '$usuario' cadastrado com sucesso</p>";
+		header("Location: index.php");
+		exit();
+	} else {
+		$_SESSION['msg'] = "<p style='color:red;'>Usuário '$usuario' não foi cadastrado com sucesso</p>";
+		header("Location: cad_usuario.php");
+		exit();
+	}
+
+	mysqli_stmt_close($stmt);
 }
+
+mysqli_close($conn);
