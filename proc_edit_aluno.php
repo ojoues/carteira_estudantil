e10bf5dbeef24d4eb147e96062b338833f59ae43
ojoues <marquesjosue81@gmail.com>
2024-02-14 19:33:35 -0300

    Ajuste da carteira
    A carteira ganhou a interface de acordo com o ano de 2024.
    
    Além disso, foram feitas melhorias para as imagens de usuários na compactação e orientação, diminuindo o tamanho do arquivo para economizar espaço e deixando sempre na orientação vertical.

--- proc_edit_aluno.php
+++ proc_edit_aluno.php
@@ -10,37 +10,52 @@ if (!isset($_SESSION['usuario_id'])) {
 include_once("conexao.php");
 $diretorio_destino = "./src/img/uploads/"; // Diretório de destino das imagens
 
-// Função para redimensionar a imagem
-function redimensionarImagem($caminho, $largura, $altura)
+// Função para redimensionar e comprimir a imagem
+function redimensionarEComprimirImagem($caminho, $largura, $altura, $qualidade)
 {
 	list($largura_original, $altura_original) = getimagesize($caminho);
 	$nova_imagem = imagecreatetruecolor($largura, $altura);
 	$imagem_original = imagecreatefromjpeg($caminho);
-	$proporcao = $largura / $altura;
-	$nova_largura = $largura_original;
-	$nova_altura = $altura_original;
-
-	if ($largura_original / $altura_original > $proporcao) {
-		$nova_largura = $altura_original * $proporcao;
-	} else {
-		$nova_altura = $largura_original / $proporcao;
-	}
-
-	$deslocamento_x = ($largura_original - $nova_largura) / 2;
-	$deslocamento_y = ($altura_original - $nova_altura) / 2;
-
-	imagecopyresampled($nova_imagem, $imagem_original, 0, 0, $deslocamento_x, $deslocamento_y, $largura, $altura, $nova_largura, $nova_altura);
-
-	return $nova_imagem;
+	imagecopyresampled($nova_imagem, $imagem_original, 0, 0, 0, 0, $largura, $altura, $largura_original, $altura_original);
+	imagejpeg($nova_imagem, $caminho, $qualidade);
+	imagedestroy($nova_imagem);
+	imagedestroy($imagem_original);
 }
 
-// Função para corrigir a orientação da imagem usando ImageMagick
+// Função para corrigir a orientação da imagem
 function corrigirOrientacaoImagem($caminho)
 {
-	if (extension_loaded('imagick')) {
-		$imagem = new Imagick($caminho);
-		$imagem->setImageOrientation(imagick::ORIENTATION_TOPLEFT);
-		$imagem->writeImage($caminho);
+	$info = @exif_read_data($caminho);
+
+	if ($info && isset($info['Orientation'])) {
+		$orientation = $info['Orientation'];
+
+		if ($orientation != 1) {
+			$image = imagecreatefromjpeg($caminho);
+
+			switch ($orientation) {
+				case 3:
+					$image = imagerotate($image, 180, 0);
+					break;
+
+				case 6:
+					$image = imagerotate($image, -90, 0);
+					break;
+
+				case 8:
+					$image = imagerotate($image, 90, 0);
+					break;
+			}
+
+			imagejpeg($image, $caminho, 20);
+			imagedestroy($image);
+		} else {
+			// Se não precisar de rotação, apenas copie o arquivo
+			copy($caminho, $caminho);
+		}
+	} else {
+		// Caso não seja possível obter informações de orientação, copie o arquivo
+		copy($caminho, $caminho);
 	}
 }
 
@@ -67,11 +82,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 		// Move o arquivo para o diretório de uploads
 		if (move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho_arquivo)) {
-			// Corrija a orientação da imagem usando ImageMagick
+			// Corrija a orientação da imagem
 			corrigirOrientacaoImagem($caminho_arquivo);
 
-			// Redimensione a imagem
-			redimensionarImagem($caminho_arquivo, 995, 1293);
+			// Redimensione e comprima a imagem
+			redimensionarEComprimirImagem($caminho_arquivo, 995, 1293, 40);
 
 			// Atualize o caminho da imagem no banco de dados
 			$atualiza_imagem = "UPDATE estudante SET imagem='$caminho_arquivo' WHERE id=$id";
@@ -114,3 +129,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$conn->close();
 	exit();
 }
+?>
--- processa.php
+++ processa.php
@@ -20,42 +20,93 @@ $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, "sssssss", $nome, $data_nascimento, $sexo, $instituicao, $curso, $cpf_sem_formato, $validade);
 
 if (mysqli_stmt_execute($stmt)) {
-	// Obtenha o ID inserido
-	$id_aluno = mysqli_insert_id($conn);
+    // Obtenha o ID inserido
+    $id_aluno = mysqli_insert_id($conn);
 
-	// Nome do arquivo original
-	$nome_arquivo_original = $_FILES['imagem']['name'];
+    // Nome do arquivo original
+    $nome_arquivo_original = $_FILES['imagem']['name'];
 
-	// Caminho completo do arquivo original no servidor
-	$caminho_arquivo_original = $diretorio_destino . $nome_arquivo_original;
+    // Caminho completo do arquivo original no servidor
+    $caminho_arquivo_original = $diretorio_destino . $nome_arquivo_original;
 
-	// Move o arquivo original para o diretório de destino
-	if (move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho_arquivo_original)) {
+    // Move o arquivo original para o diretório de destino
+    if (move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho_arquivo_original)) {
 
-		// Renomeie a imagem redimensionada
-		$novo_nome_arquivo = $id_aluno . ".jpg";
-		$caminho_arquivo = $diretorio_destino . $novo_nome_arquivo;
+        // Compactar e redimensionar a imagem nativamente em PHP
+        $novo_nome_arquivo = $id_aluno . ".jpg";
+        $caminho_arquivo = $diretorio_destino . $novo_nome_arquivo;
 
-		// Renomeie o arquivo
-		rename($caminho_arquivo_original, $caminho_arquivo);
+        // Corrige a orientação da imagem usando a biblioteca exif
+        fixImageOrientation($caminho_arquivo_original, $caminho_arquivo);
 
-		// Atualize o campo de imagem com o nome do arquivo
-		$sql_update = "UPDATE estudante SET imagem = ? WHERE id = ?";
-		$stmt_update = mysqli_prepare($conn, $sql_update);
-		mysqli_stmt_bind_param($stmt_update, "si", $caminho_arquivo, $id_aluno);
-		mysqli_stmt_execute($stmt_update);
+        // Redimensiona e comprime a imagem
+        redimensionarEComprimirImagem($caminho_arquivo, 995, 1293, 40);
 
-		$_SESSION['msg'] = "<p style='color:green;'>Aluno cadastrado com sucesso!</p>";
-		header("Location: cad_aluno");
-	} else {
-		$_SESSION['msg'] = "<p style='color:red;'>Erro ao enviar a foto.</p>";
-		header("Location: cad_aluno");
-	}
+        // Atualize o campo de imagem com o nome do arquivo
+        $sql_update = "UPDATE estudante SET imagem = ? WHERE id = ?";
+        $stmt_update = mysqli_prepare($conn, $sql_update);
+        mysqli_stmt_bind_param($stmt_update, "si", $caminho_arquivo, $id_aluno);
+        mysqli_stmt_execute($stmt_update);
 
-	// Feche as declarações
-	mysqli_stmt_close($stmt);
-	mysqli_stmt_close($stmt_update);
+        $_SESSION['msg'] = "<p style='color:green;'>Aluno cadastrado com sucesso!</p>";
+        header("Location: cad_aluno");
+    } else {
+        $_SESSION['msg'] = "<p style='color:red;'>Erro ao enviar a foto.</p>";
+        header("Location: cad_aluno");
+    }
+
+    // Feche as declarações
+    mysqli_stmt_close($stmt);
+    mysqli_stmt_close($stmt_update);
 } else {
-	$_SESSION['msg'] = "<p style='color:red;'>Erro ao cadastrar aluno: " . mysqli_error($conn) . "</p>";
-	header("Location: cad_aluno");
+    $_SESSION['msg'] = "<p style='color:red;'>Erro ao cadastrar aluno: " . mysqli_error($conn) . "</p>";
+    header("Location: cad_aluno");
 }
+
+function fixImageOrientation($source, $destination) {
+    $info = @exif_read_data($source);
+
+    if ($info && isset($info['Orientation'])) {
+        $orientation = $info['Orientation'];
+
+        if ($orientation != 1) {
+            $image = imagecreatefromjpeg($source);
+
+            switch ($orientation) {
+                case 3:
+                    $image = imagerotate($image, 180, 0);
+                    break;
+
+                case 6:
+                    $image = imagerotate($image, -90, 0);
+                    break;
+
+                case 8:
+                    $image = imagerotate($image, 90, 0);
+                    break;
+            }
+
+            imagejpeg($image, $destination, 20);
+            imagedestroy($image);
+        } else {
+            // Se não precisar de rotação, apenas copie o arquivo
+            copy($source, $destination);
+        }
+    } else {
+        // Caso não seja possível obter informações de orientação, copie o arquivo
+        copy($source, $destination);
+    }
+}
+
+// Função para redimensionar e comprimir a imagem
+function redimensionarEComprimirImagem($caminho, $largura, $altura, $qualidade)
+{
+    list($largura_original, $altura_original) = getimagesize($caminho);
+    $nova_imagem = imagecreatetruecolor($largura, $altura);
+    $imagem_original = imagecreatefromjpeg($caminho);
+    imagecopyresampled($nova_imagem, $imagem_original, 0, 0, 0, 0, $largura, $altura, $largura_original, $altura_original);
+    imagejpeg($nova_imagem, $caminho, $qualidade);
+    imagedestroy($nova_imagem);
+    imagedestroy($imagem_original);
+}
+?>
